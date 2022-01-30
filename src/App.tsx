365a97d5181742df47d9520b5e12a4e1917d59c3
Jorge Jauregui <jorgejaureguigallo@gmail.com>
2022-01-30 15:46:25 -0800

    load list of countries, render in autocomplete component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { Autocomplete, Button, TextField } from '@mui/material';
+import { Box } from '@mui/system';
 import React, { ReactElement, useEffect, useState } from 'react';
 import './App.css';
 import { getCountryGDP } from './store/actions';
-import { CountryGDP } from './store/constants';
+import { countries, CountryGDP } from './store/constants';
 
 
 const App: React.FC = (): ReactElement => {
 
   const [latestGDP, setLatestGDP] = useState<CountryGDP | undefined>(undefined)
+  const [countryAbbreviation, setCountryAbbreviation] = useState<string>("")
 
   useEffect(() => {
-    getCountryGDP("BRA").then((res) => {
+    getCountryGDP("ALB").then((res) => {
       setLatestGDP(res[0])
     })
   }, [])
@@ -17,6 +20,37 @@ const App: React.FC = (): ReactElement => {
   return (
    <div>
      {latestGDP ? <div>{latestGDP.country.value}</div> : []}
+     <Autocomplete
+      id="country-select"
+      sx={{ width: 300 }}
+      options={countries}
+      autoHighlight
+      getOptionLabel={(option) => option.label}
+      renderOption={(props, option) => (
+        <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+          <img
+            loading="lazy"
+            width="20"
+            src={`https://flagcdn.com/w20/${option.abbreviation.toLowerCase()}.png`}
+            srcSet={`https://flagcdn.com/w40/${option.abbreviation.toLowerCase()}.png 2x`}
+            alt=""
+          />
+          {option.label} ({option.abbreviation})
+        </Box>
+      )}
+      onChange={(_, v) => {if (v) {setCountryAbbreviation(v.abbreviation)}}}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Choose a country"
+          inputProps={{
+            ...params.inputProps,
+            autoComplete: 'new-password', // disable autocomplete and autofill
+          }}
+        />
+      )}
+    />
+    <Button onClick={() => {getCountryGDP(countryAbbreviation)}}>Go</Button>
    </div>
   );
 }
--- src/store/actions.ts
+++ src/store/actions.ts
@@ -2,6 +2,7 @@ import axios, { AxiosRequestConfig } from "axios";
 import { CountryGDP } from "./constants";
 
 export const getCountryGDP = async(countryAbbreviation: string): Promise<CountryGDP[]> => {
+  console.log("here", countryAbbreviation)
   const countryGDPOptions: AxiosRequestConfig = {
     method: 'GET',
     url: `https://api.worldbank.org/v2/country/${countryAbbreviation}/indicator/NY.GDP.MKTP.CD?format=json`,
